f0d72e6e4ac0dffc1f89e34559bd95026ece5893
Nico <61590265+Nicopica@users.noreply.github.com>
2026-03-19 04:05:29 +0100

    Delete Project directory

--- Project/sql/mesurements.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS measurements (
-    measurement_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    sensor_id INTEGER NOT NULL,
-    type_id INTEGER NOT NULL,
-    timestamp TEXT NOT NULL,
-    value REAL NOT NULL,
-    FOREIGN KEY (sensor_id) REFERENCES sensors (sensor_id) ON DELETE CASCADE,
-    FOREIGN KEY (type_id) REFERENCES sensor_types (type_id) ON DELETE CASCADE
-);
--- Project/sql/sensor.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS sensors (
-    sensor_id INTEGER PRIMARY KEY,
-    latitude REAL NOT NULL,
-    longitude REAL NOT NULL
-);
